e1de84d4709a15f066f1eccf761ea29e4ca9f11e
ANTONIO <joelant97@gmail.com>
2018-12-13 19:46:35 -0400

    Services Security + StorageProperties

--- src/main/java/electronicapractica10/demo/DemoApplication.java
+++ src/main/java/electronicapractica10/demo/DemoApplication.java
@@ -1,14 +1,39 @@
 package electronicapractica10.demo;
 
+import electronicapractica10.demo.service.StorageProperties;
+import electronicapractica10.demo.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+
+
+
+@EnableAutoConfiguration
+@ComponentScan
+@EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
+
+
+
 
