7d25c2239b6144c9082f117e9cdd70434f99d420
ANTONIO <joelant97@gmail.com>
2018-12-17 17:24:30 -0400

    Configuraciones del Server

---
+++ src/main/java/electronicapractica10/demo/configuracion/ResourceServerConfig.java
@@ -0,0 +1,102 @@
+package electronicapractica10.demo.configuracion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+import javax.sql.DataSource;
+
+@Configurable
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.
+                jdbcAuthentication()
+                .usersByUsernameQuery(usersQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    /*
+     * Permite configurar las reglas de seguridad.
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Marcando las reglas para permitir unicamente los usuarios
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/img/**").permitAll() //permitiendo llamadas a esas urls.
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/**").hasAnyAuthority("ADMIN", "USER")
+                //.anyRequest().authenticated() //cualquier llamada debe ser validada
+                .and()
+                .formLogin()
+                .loginPage("/login") //indicando la ruta que estaremos utilizando.
+                .failureUrl("/login?error") //en caso de fallar puedo indicar otra pagina.
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll();
+
+
+        //deshabilitando las seguridad contra los frame
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
